d45b3ef150595244a529e48a6920d65e923881b8
Склизков Артём <L4ckyHop>
2025-05-22 11:42:49 +0200

    Добавление функции воспроизведения

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 //Инициализация элементов
-const audio = document.getElementById('audioElement');
-const progress = document.getElementById('progress');
-const speedSelect = document.getElementById('speedSelect');
+const audio = document.getElementById('audioElement');//аудио
+const progress = document.getElementById('progress');//Момент из аудио
+const speedSelect = document.getElementById('speedSelect');//Выбор скорости
+//Изменение скорости
+function changeSpeed(speed) {//Функция
+    audio.playbackRate = parseFloat(speed);
+}
