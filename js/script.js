612669ba2014b758b79127563c6e58140b01556c
Склизков Артём <L4ckyHop>
2025-05-22 11:38:23 +0200

    Инициализация элементов

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//Инициализация элементов
+const audio = document.getElementById('audioElement');
+const progress = document.getElementById('progress');
+const speedSelect = document.getElementById('speedSelect');
